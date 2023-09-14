e7efc723370667d054b8b786a15c999a5069b557
Oleh Kovalets <olegkovalets1112003@gmail.com>
2023-09-14 22:30:24 +0300

    Update api class

--- src/js/API/tasty-treats-api.js
+++ src/js/API/tasty-treats-api.js
@@ -5,15 +5,31 @@ export default class TastyTreatsAPI {
     this.baseURL = 'https://tasty-treats-backend.p.goit.global/api';
   }
 
+  async fetchEvents() {
+    return await axios.get(`${this.baseURL}/events`);
+  }
+
   async fetchAllRecipes() {
     return await axios.get(`${this.baseURL}/recipes?limit=9`);
+  }
+  
+  async fetchCategories() {
+    return await axios.get(`${this.baseURL}/categories`);
   }
 
   async fetchPopularRecipes() {
     return await axios.get(`${this.baseURL}/recipes/popular`);
   }
 
-  async fetchCategories() {
-    return await axios.get(`${this.baseURL}/categories`);
+  async fetchAreas() {
+    return await axios.get(`${this.baseURL}/areas`);
+  }
+
+  async fetchIngrediens() {
+    return await axios.get(`${this.baseURL}/ingredients`);
+  }
+
+  async fetchRecipe(id) {
+    return await axios.get(`${this.baseURL}/recipes/${id}`);
   }
 }
