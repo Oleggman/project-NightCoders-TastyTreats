5e395c2f282e1dd43022be20496f96ba27c43e4b
Anton Rybalko <126842693+AntonRybalko777@users.noreply.github.com>
2023-09-17 02:00:02 +0300

    ddd

--- src/js/partials/all-recipes.js
+++ src/js/partials/all-recipes.js
@@ -2,35 +2,48 @@ import TastyTreatsAPI from '../API/tasty-treats-api.js';
 import { renderGallery } from '../renders/render-gallery.js';
 
 const allRecipesRender = new TastyTreatsAPI();
-const gallery = document.querySelector('.gallery-container');
+const gallery = document.querySelector('.cards-container');
+a();
 
 async function a() {
   const res = await allRecipesRender.fetchAllRecipes();
   gallery.innerHTML = renderGallery(res.data.results);
 }
 
-a();
+let favorites = JSON.parse(localStorage.getItem('favorites')) ?? [];
+const idList = favorites.map(el => el.id);
+console.log(idList);
 
-const favorites = [];
+// for (let i = 0; i < 10; i++) {
+//   if (idList.includes(cards[i].id)) {
+//     cards[i].classList.add('like-favorite');
+//   }
+// }
 
 gallery.addEventListener('click', handlerLike);
 
 function handlerLike(evt) {
+  const svg = evt.target.firstElementChild;
+  const li = evt.target.parentNode;
   if (evt.target.classList.contains('like-button')) {
-    if (evt.target.firstElementChild.classList.contains('like-favorite')) {
-      evt.target.firstElementChild.classList.remove('like-favorite');
+    if (svg.classList.contains('like-favorite')) {
+      svg.classList.remove('like-favorite');
+      for (let i = 0; i < favorites.length; i++) {
+        if (favorites[i].id === li.id) {
+          const _ = favorites.splice(i, 1);
+        }
+      }
     } else {
-      console.dir(evt.target.parentNode);
       favorites.push({
-        category: evt.target.parentNode.dataset.category,
-        description: ' ',
-        id: ' ',
-        preview: ' ',
-        rating: ' ',
-        title: ' ',
+        title: li.dataset.title,
+        category: li.dataset.category,
+        description: li.dataset.description,
+        id: li.id,
+        preview: li.dataset.preview,
+        rating: li.dataset.rating,
       });
-      localStorage.setItem('favorites', JSON.stringify(favorites));
-      evt.target.firstElementChild.classList.add('like-favorite');
+      svg.classList.add('like-favorite');
     }
+    localStorage.setItem('favorites', JSON.stringify(favorites));
   }
 }
