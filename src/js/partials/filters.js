b3edb69610e0792f4d148732727895f622600828
Олег <olegkovalets1112003@knu.ua>
2023-09-16 17:24:57 +0300

    Create render by ingredients  filter script

--- src/js/partials/filters.js
+++ src/js/partials/filters.js
@@ -7,10 +7,12 @@ const tastyTreatsApi = new TastyTreatsAPI();
 refs.recipeInput.addEventListener('input', debounce(onInputRecipe, 300));
 refs.timeSelect.addEventListener('change', onTimeSelect);
 refs.areaSelect.addEventListener('change', onAreaSelect);
+refs.ingredSelect.addEventListener('change', onIngredSelect);
 
 refs.timeSelect.innerHTML = renderTimeOptions();
 loadAreaOptions();
-
+loadIngredOptions();
+// Input filter
 async function onInputRecipe(e) {
   const res = await tastyTreatsApi.fetchAllRecipes();
   const dataArr = res.data.results;
@@ -21,7 +23,7 @@ async function onInputRecipe(e) {
 
   refs.gallery.innerHTML = renderGallery(filteredByInput);
 }
-
+// Time filter
 async function onTimeSelect(e) {
   const selectedTime = Number(e.currentTarget.value);
   const res = await tastyTreatsApi.fetchAllRecipes();
@@ -35,7 +37,7 @@ async function onTimeSelect(e) {
 
   refs.gallery.innerHTML = renderGallery(filteredByTime);
 }
-
+// Area filter
 async function loadAreaOptions() {
   const res = await tastyTreatsApi.fetchAreas();
   refs.areaSelect.innerHTML = renderOptions(res.data);
@@ -48,4 +50,24 @@ async function onAreaSelect(e) {
 
   const recipesByArea = dataArr.filter(item => item.area === value);
   refs.gallery.innerHTML = renderGallery(recipesByArea);
+}
+// Ingredients filter
+loadIngredOptions();
+async function loadIngredOptions() {
+  const res = await tastyTreatsApi.fetchIngrediens();
+  refs.ingredSelect.innerHTML = renderOptions(res.data);
+}
+
+async function onIngredSelect(e) {
+  const ingreds = await tastyTreatsApi.fetchIngrediens();
+  const ingredId = ingreds.data.find(item => item.name === e.target.value)._id;
+
+  const res = await tastyTreatsApi.fetchAllRecipes();
+  dataArr = res.data.results;
+
+  const recipesByIngreds = dataArr.filter(item =>
+    item.ingredients.some(ingr => ingr.id === ingredId)
+  );
+
+  refs.gallery.innerHTML = renderGallery(recipesByIngreds);
 }
--- src/js/refs.js
+++ src/js/refs.js
@@ -1,6 +1,8 @@
 const refs = {
-  recipeInput: document.querySelector('js-recipe-input'),
+  recipeInput: document.querySelector('.js-recipe-input'),
+  timeSelect: document.querySelector('.js-time-select'),
   areaSelect: document.querySelector('.js-area-select'),
+  ingredSelect: document.querySelector('.js-ingred-select'),
   gallery: document.querySelector('.js-gallery'),
 };
 
