a3e1eb738959e4d8664620df83b8bdd2fbe79731
MorskoySom <Somovvitvit@gmail.com>
2023-09-17 00:00:00 +0300

    123

--- src/js/renders/render-gallery.js
+++ src/js/renders/render-gallery.js
@@ -55,15 +55,7 @@ function renderGallery(data) {
 }
 
 function renderPopular(data) {
-  return data.map(item => `<li class="car-container">
-        <div class="picture">
-            <img src="${data.preview}" alt="${data.title}">
-        </div>        
-        <div class="text-container">
-            <div class="popular-name">${data.title}</div>
-            <p class="popular-desc desc">${data.description}</p>
-        </div>        
-    </li>`).join('');
+  return data.map(item => `<!-- Розмітка лішки популярних -->`).join('');
 }
 
 function renderOptions(data) {
