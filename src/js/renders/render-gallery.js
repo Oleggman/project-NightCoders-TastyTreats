6f12cfbabc810b942ec767b88b9b3569a1d1c490
MaksymGS <130307586+MaksymGS@users.noreply.github.com>
2023-09-17 13:45:16 +0300

    first commit

--- src/js/renders/render-gallery.js
+++ src/js/renders/render-gallery.js
@@ -62,8 +62,45 @@ function renderOptions(data) {
   return data.map(item => `<!-- Розмітка кнопки категорії -->`).join('');
 }
 
-function renderEvents() {
-  return data.map(item => `<!-- Розмітка івенту в герої -->`).join('');
+function renderEvents(data) {
+  return data
+    .map(
+      ({
+        cook:{ imgWebpUrl, name: cook },
+        topic: { name, area, previewWebpUrl, imgWebpUrl: imgDish },
+      }) => `        
+      
+  <!-- Slides -->
+  <div class="swiper-slide">
+    <div class="slide-wrapper">
+      <div class="block-cook">
+        <img
+          src="${imgWebpUrl}"
+          alt="${cook}"
+        />
+      </div>
+      <div class="block-dish">
+        <img
+          src="${previewWebpUrl}"
+          alt="${name}"
+        />
+        <div class="block-dish-ellipse"></div>
+        <h3 class="block-dish-descr">${name}</h3>
+        <p class="block-dish-area">${area}</p>
+      </div>
+      <div class="block-dish-image">
+        <img
+          src="${imgDish}"
+          alt="${name}"
+        />
+      </div>
+    </div>
+  </div>
+  
+  </div>
+  `
+    )
+    .join('');
 }
 
 export { renderGallery, renderPopular, renderOptions, renderEvents };
