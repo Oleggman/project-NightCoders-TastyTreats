e65b14c13e6f842e58dcbf28da40cefdd507c494
YuliaSilk <129064716+YuliaSilk@users.noreply.github.com>
2023-09-17 21:13:49 +0300

    update modal-recipes.js

--- src/js/modal-recipes.js
+++ src/js/modal-recipes.js
@@ -1,12 +1,14 @@
 import TastyTreatsAPI from "../API/tasty-treats-api";
 
+const myTastyTreatsApi = new TastyTreatsAPI();
+
 
 const modal = document.querySelector(".modal-recipes-container");
 const closeModalBtn = document.querySelector(".modal-close-btn");
 const containerIngredients = document.querySelector(".ingredients");
 const containerSelected = document.querySelector(".selected");
-const name = document.querySelector(".name-of-recipe");
-const recipeText = document.querySelector("");
+const recipeNameElement = document.querySelector(".name-of-recipe");
+const recipeText = document.querySelector(".recipe-text-box");
 
 const videoContainer = document.getElementById("recipeVideo");
 const playBtn = document.getElementById("playPause");
@@ -44,10 +46,10 @@ loadVideo();
         function playVideo () {
         if(!video.paused) {
             video.pause();
-        } else(!video.played) {
-            video.play();
+        } else(video.play()) 
+            
         }
-    }
+    
 
     // function stopVideo() {
     //     video.currentTime = 0;
@@ -61,47 +63,111 @@ loadVideo();
 
 function loadRecipeName() {
     const titleUrl = "https://tasty-treats-backend.p.goit.global/api/recipes?title";
-    
-}
+    fetch(titleUrl)
+    .then(response => response.json())
+    .then(data => {
+        const recipeName = data.recipeName;
+        recipeNameElement.textContent = recipeName; 
+    })
+    .catch(error => {
+        console.error("Sory! Error loading recipe name:", error);
+    })
+}   
+loadRecipeName()
 
-// title
+function createAndInsertElement(data) {
+    const newElement = document.createElement("div");
+    newElement.textContent = data.recipeName;
+    document.body.appendChild(newElement);
+// add class
+    newElement.classList.add('name-of-recipe');
+    return newElement;
+}
+const newRecipeName = {
+    recipeName: `${title}`
+};
+const createdElement = createAndInsertElement(newRecipeName)
 
 // СПИСОК ІНРЕДІЄНТІВ
-
-function createMarkup(arr) {
-    return arr.map(({ ingredients.id, ingredients.measure}) => `
-    <ul class="ingredients">
-    <li class="product-name">
-        <p>"${ingredients.id}"</p>
-    </li>
-        <li class="number">
-            <p>"${ingredients.measure}"</p>
+function renderIngredientsList(ingredients) {
+    const markup = ingredients
+      .map(
+        (ingredient) => `<ul class="ingredients">
+        <li class="product-name">
+            <p>"${ingredients.id}"</p>
         </li>
-</ul> `).join('')
-}
-containerIngredients.insertAdjacentHTML('beforeend', createMarkup(ingredients));
+            <li class="number">
+                <p>"${ingredients.measure}"</p>
+            </li>
+    </ul> `
+      )
+      .join("");
+      ingredients.innerHTML = markup;
+  }
+  renderIngredientsList()
+
+
+
+// function createMarkup(arr) {
+//     return arr.map(({ ingredients.id, ingredients.measure}) => `
+//     <ul class="ingredients">
+//     <li class="product-name">
+//         <p>"${ingredients.id}"</p>
+//     </li>
+//         <li class="number">
+//             <p>"${ingredients.measure}"</p>
+//         </li>
+// </ul> `).join('')
+// }
+// containerIngredients.insertAdjacentHTML('beforeend', createMarkup(ingredients));
 
 
 // ОБРАНІ КАТЕГОРІЇ (хештеги)
 function createMarkup(arr) {
-    return arr.map(({ }) => `
+    return arr.map(({ category }) => `
     <li>
-      <p class="categiries">"#${}"</p>
+      <p class="categiries">"#${category}"</p>
      </li> `).join('')
 }
 containerSelected.insertAdjacentHTML('beforeend', createMarkup(selected));
 
 
 // ТЕКСТ РЕЦЕПТУ
-const fetchRecipes = async name => {
-    const response = await fetch(`${this.baseURL}/recipes/${id}`)
-    return await response.json();
+// 
+function loadRecipeInstruction() {
+    const instructionUrl = "https://tasty-treats-backend.p.goit.global/api/recipes?instructions";
+ fetch(instructionUrl)
+ .then(response => response.text())
+ .then(data => {
+    const recipeInstructions = data.instructions;
+    recipeText.textContent = recipeInstructions;
+ })
+ .catch(error => {
+    console.error("Error loading and inserting recipe instructions:", error);
+ });
 }
-// text() - парсить дані в простому текстовому форматі, наприклад .csv (табличні дані).
+loadRecipeInstruction();
+
 
 // async fetchRecipe(id) {
 //     return await axios.get(`${this.baseURL}/recipes/${id}`);
 //   } 
+
+
+
+// function renderUserListItems(users) {
+//     const markup = users
+//       .map(
+//         (user) => `<li class="item">
+//           <p><b>Name</b>: ${user.name}</p>
+//           <p><b>Email</b>: ${user.email}</p>
+//           <p><b>Company</b>: ${user.company.name}</p>
+//         </li>`
+//       )
+//       .join("");
+//     userList.innerHTML = markup;
+//   }
+
 
 
 // КНОПКИ
