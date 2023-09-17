f85271df7ee4e84e6a86b89f6a3a63e4b6d1dfd4
Anton Rybalko <126842693+AntonRybalko777@users.noreply.github.com>
2023-09-17 16:17:59 +0300

    fixed svg

--- src/js/renders/render-gallery.js
+++ src/js/renders/render-gallery.js
@@ -2,12 +2,12 @@ function renderGallery(data) {
   return data
     .map(
       ({ title, description, preview, rating, _id, category, area }) => `<li
-      class="card-container" id=${_id} data-category="${category}" data-area=${area}
+      class="card-container" id="${_id}" data-category="${category}" data-area="${area}" data-title="${title}" data-description="${description}" data-preview="${preview}" data-rating="${rating}"
       style="background-image: linear-gradient(1deg,rgba(5, 5, 5, 0.6),rgba(5, 5, 5, 0)),
       url(${preview});">
       <button class="like-button">
         <svg class="like-icon" width="22" height="22">
-          <use href="./img/icons.svg#heart"></use>
+          <use href="/src/img/icons.svg#heart"></use>
         </svg>
         </button>
       <div class="card-info">
@@ -19,29 +19,34 @@ function renderGallery(data) {
           <div class="card-rate">
             <span class="card-rate-value">${rating.toFixed(1)}</span>
             <div class="card-rate-stars">
-              <svg class=${rating.toFixed(1) >= 1 ? 'star-icon-orange' : 'star-icon-grey'
-        }>
-                <use href="./img/icons.svg#star"></use>
+              <svg class=${
+                rating.toFixed(1) >= 1 ? 'star-icon-orange' : 'star-icon-grey'
+              }>
+                <use href="/src/img/icons.svg#star"></use>
               </svg>
-              <svg class=${rating.toFixed(1) >= 2 ? 'star-icon-orange' : 'star-icon-grey'
-        }>
-                <use href="./img/icons.svg#star"></use>
+              <svg class=${
+                rating.toFixed(1) >= 2 ? 'star-icon-orange' : 'star-icon-grey'
+              }>
+                <use href="/src/img/icons.svg#star"></use>
               </svg>
-              <svg class=${rating.toFixed(1) >= 3 ? 'star-icon-orange' : 'star-icon-grey'
-        }>
-                <use href="./img/icons.svg#star"></use>
+              <svg class=${
+                rating.toFixed(1) >= 3 ? 'star-icon-orange' : 'star-icon-grey'
+              }>
+                <use href="/src/img/icons.svg#star"></use>
               </svg>
-              <svg class=${rating.toFixed(1) >= 4 ? 'star-icon-orange' : 'star-icon-grey'
-        }>
-                <use href="./img/icons.svg#star"></use>
+              <svg class=${
+                rating.toFixed(1) >= 4 ? 'star-icon-orange' : 'star-icon-grey'
+              }>
+                <use href="/src/img/icons.svg#star"></use>
               </svg>
-              <svg class=${rating.toFixed(1) >= 5 ? 'star-icon-orange' : 'star-icon-grey'
-        }>
-                <use href="./img/icons.svg#star"></use>
+              <svg class=${
+                rating.toFixed(1) >= 5 ? 'star-icon-orange' : 'star-icon-grey'
+              }>
+                <use href="/src/img/icons.svg#star"></use>
               </svg>
             </div>
           </div>
-          <button class="card-footer-btn">See recipe</button>
+          <button class="card-footer-btn" data-id="${_id}">See recipe</button>
         </div>
       </div>
     </li>`
@@ -50,7 +55,9 @@ function renderGallery(data) {
 }
 
 function renderPopular(data) {
-  return data.map(item => `<li class="car-container">
+  return data
+    .map(
+      item => `<li class="car-container">
         <div class="picture">
             <img src="${item.preview}" alt="${item.title}">
         </div>        
@@ -58,7 +65,9 @@ function renderPopular(data) {
             <div class="popular-name">${item.title}</div>
             <p class="popular-desc desc">${item.description}</p>
         </div>        
-    </li>`).join('');
+    </li>`
+    )
+    .join('');
 }
 
 function renderCategories(data) {
@@ -75,7 +84,7 @@ function renderEvents(data) {
   return data
     .map(
       ({
-        cook:{ imgWebpUrl, name: cook },
+        cook: { imgWebpUrl, name: cook },
         topic: { name, area, previewWebpUrl, imgWebpUrl: imgDish },
       }) => `        
       
@@ -112,4 +121,10 @@ function renderEvents(data) {
     .join('');
 }
 
-export { renderGallery, renderPopular, renderCategories, renderOptions, renderEvents };
+export {
+  renderGallery,
+  renderPopular,
+  renderCategories,
+  renderOptions,
+  renderEvents,
+};
