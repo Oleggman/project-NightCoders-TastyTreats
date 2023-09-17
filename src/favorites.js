713a6bd0e8eff4c85ca38c9057bcebbde38053a4
TalyaMonya <128909188+TalyaMonya@users.noreply.github.com>
2023-09-17 18:27:59 +0300

    Favorites-js

--- src/favorites.js
+++ src/favorites.js
@@ -1,9 +1,1 @@
-
-
-const ref = {
-    favoriteCategoriesList: document.querySelector('.favorite-categories-btn'),
-    favoriteRecipesList: document.querySelector('.favorites-list'),
-    favoritesDefault: document.querySelector('.favorites-default'),
-    allBtn: document.querySelector('.all-btn'),
-};
-
+import './js/partials/favorites-page';
