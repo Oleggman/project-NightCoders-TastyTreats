67595b2d0189315434c8226720184b4f434d4ae1
Oleh Kovalets <olegkovalets1112003@gmail.com>
2023-09-19 01:55:53 +0300

    Render ingredients in recipe modal

--- src/js/partials/modals/modal-recipes.js
+++ src/js/partials/modals/modal-recipes.js
@@ -2,6 +2,7 @@ const modalRefs = {
   titles: document.querySelectorAll('.recipe-modal-title'),
   video: document.querySelector('.video-container'),
   description: document.querySelector('.recipe-text'),
+  ingredients: document.querySelector('.recipe-modal-ingreds'),
 }
 
 loadModal();
@@ -11,6 +12,11 @@ async function loadModal(recipe) {
   modalRefs.video.setAttribute('src', recipe.youtube);
 
   modalRefs.description.textContent = recipe.instructions;
+  modalRefs.ingredients.innerHTML = recipe.ingredients.map(ingred => `
+    <li class="modal-recipe-ingred">
+      <span>${ingred.name}</span><span>${ingred.measure}</span>
+    </li>
+  `).join('');
 }
 
 export { loadModal };
