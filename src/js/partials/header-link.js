636182cf60b177930c560e76939fb7fafe30a6be
Oleh Kovalets <olegkovalets1112003@gmail.com>
2023-09-18 22:13:23 +0300

    Fix header link

--- src/js/partials/header-link.js
+++ src/js/partials/header-link.js
@@ -2,11 +2,9 @@
 
 const navigationLinks = document.querySelectorAll('.link-list-header');
 const currentPath = window.location.pathname;
-    console.log(currentPath);
 
     const navigationLinksArray = Array.from(navigationLinks);
     const hasActiveLink = navigationLinksArray.some(link => link.getAttribute('href') === currentPath);
-console.log(hasActiveLink);
 
 if (!hasActiveLink) {
     navigationLinksArray.forEach(link => {
@@ -16,7 +14,7 @@ if (!hasActiveLink) {
     });
 } else {
     navigationLinks.forEach(link => {
-        if (link.getAttribute('href') === currentPath) {
+        if (link.getAttribute('href') === '.' + currentPath) {
             link.classList.add('active-page');
         } else {
             link.classList.remove('active-page');
