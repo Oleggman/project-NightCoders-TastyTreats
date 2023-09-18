df4d932c8b612344057b98df3debf65c38747c55
Oleh Kovalets <olegkovalets1112003@gmail.com>
2023-09-19 02:05:41 +0300

    Render tags in recipe modal

--- src/js/partials/modals/modal-recipes.js
+++ src/js/partials/modals/modal-recipes.js
@@ -1,8 +1,9 @@
 const modalRefs = {
   titles: document.querySelectorAll('.recipe-modal-title'),
   video: document.querySelector('.video-container'),
-  description: document.querySelector('.recipe-text'),
+  tags: document.querySelector('.recipe-modal-tags'),
   ingredients: document.querySelector('.recipe-modal-ingreds'),
+  description: document.querySelector('.recipe-text'),
 }
 
 loadModal();
@@ -10,13 +11,18 @@ async function loadModal(recipe) {
   console.log(recipe)
   modalRefs.titles.forEach(title => title.textContent = recipe.title);
   modalRefs.video.setAttribute('src', recipe.youtube);
-
-  modalRefs.description.textContent = recipe.instructions;
+  
+  modalRefs.tags.innerHTML = recipe.tags.map(tag => `
+    <li>
+      <p class="categiries">#${tag}</p>
+    </li>`).join('');
+  
   modalRefs.ingredients.innerHTML = recipe.ingredients.map(ingred => `
     <li class="modal-recipe-ingred">
       <span>${ingred.name}</span><span>${ingred.measure}</span>
-    </li>
-  `).join('');
+    </li>`).join('');
+  
+  modalRefs.description.textContent = recipe.instructions;
 }
 
 export { loadModal };
