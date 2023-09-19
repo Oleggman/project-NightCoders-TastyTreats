5c53af1a35e959bedde6061f17c8ac4ce195e797
Anton Rybalko <126842693+AntonRybalko777@users.noreply.github.com>
2023-09-20 01:55:40 +0300

    1

--- src/js/open-modal/open-close-modals.js
+++ src/js/open-modal/open-close-modals.js
@@ -40,7 +40,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
       modalElem.classList.add('active');
       overlay.classList.add('active');
-      document.body.style.overflow = "hidden";
+      document.body.style.overflow = 'hidden';
     });
   });
 
@@ -50,27 +50,27 @@ document.addEventListener('DOMContentLoaded', function () {
 
       parentModal.classList.remove('active');
       overlay.classList.remove('active');
-      document.body.style.overflow = "auto";
+      document.body.style.overflow = 'auto';
     });
   });
 
-  document.body.addEventListener(
-    'keyup',
-    function (e) {
-      var key = e.keyCode;
+  // document.body.addEventListener(
+  //   'keyup',
+  //   function (e) {
+  //     var key = e.keyCode;
 
-      if (key == 27) {
-        document.querySelector('.modal.active').classList.remove('active');
-        document.querySelector('.overlay').classList.remove('active');
-        document.body.style.overflow = "auto";
-      }
-    },
-    false
-  );
+  //     if (key == 27) {
+  //       document.querySelector('.modal.active').classList.remove('active');
+  //       document.querySelector('.overlay').classList.remove('active');
+  //       document.body.style.overflow = "auto";
+  //     }
+  //   },
+  //   false
+  // );
 
   overlay.addEventListener('click', function () {
     document.querySelector('.modal.active').classList.remove('active');
     this.classList.remove('active');
-    document.body.style.overflow = "auto";
+    document.body.style.overflow = 'auto';
   });
 });
