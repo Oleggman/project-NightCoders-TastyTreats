2e27595d41fcb62054e7bafc730da36febf3f3b4
Anton Rybalko <126842693+AntonRybalko777@users.noreply.github.com>
2023-09-20 01:23:16 +0300

    1

---
+++ src/js/partials/scroll-up.js
@@ -0,0 +1,11 @@
+const scrollUp = document.querySelector('.scroll-icon');
+
+window.onscroll = () => {
+  if (window.scrollY > 300) {
+    scrollUp.classList.remove('scroll-hidden');
+  } else {
+    scrollUp.classList.add('scroll-hidden');
+  }
+};
+
+scrollUp.onclick = () => {};
