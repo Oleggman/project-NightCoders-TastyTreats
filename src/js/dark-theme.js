00383f880a6e9e40b9cb59faac201c6c20dfb3c4
Oleh Kovalets <olegkovalets1112003@gmail.com>
2023-09-21 18:02:50 +0300

    Add dark theme to localStoradge

--- src/js/dark-theme.js
+++ src/js/dark-theme.js
@@ -2,9 +2,18 @@ const toggles = document.querySelectorAll('.theme-toggle');
 
 toggles.forEach(toggle => toggle.addEventListener('change', onChangeTheme));
 
+const storadgeTheme = localStorage.getItem("ui-theme");
+if (storadgeTheme) {
+  document.documentElement.setAttribute('data-theme', JSON.parse(storadgeTheme));
+  toggles.forEach(toggle => toggle.checked = true);
+} else {
+  document.documentElement.setAttribute('data-theme', 'light');
+}
+
 function onChangeTheme(e) {
   const currentTheme = document.documentElement.getAttribute('data-theme');
   const targetTheme = currentTheme === 'light' ? 'dark' : 'light';
 
   document.documentElement.setAttribute('data-theme', targetTheme);
+  localStorage.setItem("ui-theme", JSON.stringify(targetTheme));
 }
