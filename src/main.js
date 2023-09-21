d3e643e6673ab12c1ff75cc290c88242d8219fc3
MaksymGS <130307586+MaksymGS@users.noreply.github.com>
2023-09-22 00:06:46 +0300

    all rec test

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import './js/partials/header-link';
-import './js/partials/gallery/filters.js';
 import './js/partials/hero.js';
+import './js/partials/gallery/filters.js';
 import './js/partials/gallery/all-recipes.js';
 import './js/partials/gallery/popular.js';
 import './js/partials/gallery/load-filter-options.js';
