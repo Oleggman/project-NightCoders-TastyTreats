04a915b877a6f99340956396954b3939c04fdbec
Олег <olegkovalets1112003@knu.ua>
2023-09-22 00:56:52 +0300

    Try fix 5

--- src/main.js
+++ src/main.js
@@ -24,6 +24,8 @@ import './js/partials/gallery/filters.js';
 import './js/partials/gallery/all-categories.js';
 import './js/partials/modals/modal-recipes.js';
 import './js/open-modal/open-modal.js';
+import './js/open-modal/open-close-modals.js';
+import './js/partials/pop-up-rating.js';
 import './js/partials/footer.js';
 import './js/partials/scroll-up.js';
 import './js/partials/modals/order-now.js';
