f7d452677b7d368e9b9d32b87312e1dd9c8323c2
Anton Rybalko <126842693+AntonRybalko777@users.noreply.github.com>
2023-09-22 15:26:50 +0300

    working on post method

--- src/js/partials/modals/order-now.js
+++ src/js/partials/modals/order-now.js
@@ -1,4 +1,5 @@
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import axios from 'axios';
 
 const overlay = document.querySelector('.js-overlay-modal');
 const orderForm = document.querySelector('.order-now-form');
@@ -7,11 +8,29 @@ orderForm.addEventListener('submit', onSubmitOrder);
 
 function onSubmitOrder(e) {
   e.preventDefault();
-  e.currentTarget.reset();
-  Notify.success('The form has been sent successfully! A manager will contact you shortly.');
-  
+
   var parentModal = this.closest('.modal');
   parentModal.classList.remove('active');
   overlay.classList.remove('active');
-  document.body.style.overflow = "auto";
+  document.body.style.overflow = 'auto';
+
+  const dataPost = {
+    name: orderForm.elements.name.value,
+    phone: orderForm.elements.phone.value,
+    email: orderForm.elements.email.value,
+    comment: orderForm.elements.comment.value,
+  };
+  console.log(orderForm.elements.email.value);
+
+  axios
+    .post('https://tasty-treats-backend.p.goit.global/api/orders/add', dataPost)
+    .then(() => {
+      Notify.success(
+        'The form has been sent successfully! A manager will contact you shortly.'
+      );
+    })
+    .catch(function (error) {
+      console.log(error.message);
+    });
+  e.currentTarget.reset();
 }
